11cd0d46b4164ceaf2344c498b4d70e1114201b9
Wan Ismat <ismatazmy@live.com>
2021-03-18 12:19:02 +0800

    Some small changes, added 1 attributes.

--- samples/src/samples/demo/Location.java
+++ samples/src/samples/demo/Location.java
@@ -2,6 +2,7 @@ package samples.demo;
 
 public class Location {
 	private String fname;
+	private String lname = "Ismaty";
 
 	public String getFname() {
 		return fname;
@@ -9,5 +10,11 @@ public class Location {
 
 	public void setFname(String fname) {
 		this.fname = fname;
+	}
+	
+	public static void main(String[] args) {
+		Location lname_loc = new Location();
+		
+	    System.out.println(lname_loc.lname);
 	}
 }
