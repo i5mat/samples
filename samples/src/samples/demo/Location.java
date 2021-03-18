124115da2fdd83f427d25120765a41532da00b26
Wan Ismat <ismatazmy@live.com>
2021-03-18 12:13:55 +0800

    Added Location.java in samples.demo package. Setter and getter with Private attribute set.

---
+++ samples/src/samples/demo/Location.java
@@ -0,0 +1,13 @@
+package samples.demo;
+
+public class Location {
+	private String fname;
+
+	public String getFname() {
+		return fname;
+	}
+
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+}
